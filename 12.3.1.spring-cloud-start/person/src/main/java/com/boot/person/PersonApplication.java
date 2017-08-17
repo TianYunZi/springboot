6a04be17d4fc79d13e75d4b6edd162eefa8c81e9
zxysegh@gmail.com <20150130TUESDAy>
2017-08-18 07:33:51 +0800

    demo test

--- 12.3.1.spring-cloud-start/person/src/main/java/com/boot/person/PersonApplication.java
+++ 12.3.1.spring-cloud-start/person/src/main/java/com/boot/person/PersonApplication.java
@@ -7,9 +7,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class PersonApplication {
-	
-	   public static void main(String[] args) {
-	        SpringApplication.run(PersonApplication.class, args);
-	    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApplication.class, args);
+        try {
+            Class<?> clazz = Class.forName("com.boot.person.domain.Person");
+            Object newInstance = clazz.newInstance();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
